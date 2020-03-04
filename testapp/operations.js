cb1ddebdd2cf392c25aff11bcaa309943a9b14ca
제인도 <glitchfishzero@gmail.com>
2020-03-04 17:12:26 +0500

    feat: Testing. Assert module

--- testapp/operations.js
+++ testapp/operations.js
@@ -1,13 +1,13 @@
 module.exports.multiply = function(x, y) {
   return x * y;
 };
-module.exports.add = function(x, y) {
-  return x + y;
-};
+// module.exports.add = function(x, y) {
+//   return x + y;
+// };
 
-//* Тестирование асинхронных функций
-module.exports.multiplyAsync = function(a, b, callback) {
-  setTimeout(function() {
-    callback(a * b);
-  }, 1000);
-};
+// //* Тестирование асинхронных функций
+// module.exports.multiplyAsync = function(a, b, callback) {
+//   setTimeout(function() {
+//     callback(a * b);
+//   }, 1000);
+// };
--- testapp/operations.test.js
+++ testapp/operations.test.js
@@ -1,12 +1,14 @@
-var operations = require('./operations');
+// TODO: Mocha
 
-it('should multiply two numbers', function() {
-  var expectedResult = 15; // change figures to fail test
-  var result = operations.multiply(3, 5);
-  if (result !== expectedResult) {
-    throw new Error(`Expected ${expectedResult}, but got ${result}`);
-  }
-});
+// var operations = require('./operations');
+
+// it('should multiply two numbers', function() {
+//   var expectedResult = 15; // change figures to fail test
+//   var result = operations.multiply(3, 5);
+//   if (result !== expectedResult) {
+//     throw new Error(`Expected ${expectedResult}, but got ${result}`);
+//   }
+// });
 
 /*
 Рассмотрим этот тест. Для тестирования результата применяется функция it(), которая предоставляется фреймворком Mocha.
@@ -16,23 +18,23 @@ it('should multiply two numbers', function() {
 К примеру, нам надо проверить работу выше определенной функции multiply(), которая умножает два числа. Для этого в эту функцию надо передать два числа и сравнить ее результат с ожидаемым. Если результат не совпадает с ожидаемым значением, то генерируется ошибка.
 */
 
-it('should add two numbers', function() {
-  var expectedResult = 16;
-  var result = operations.add(9, 7);
-  if (result !== expectedResult) {
-    throw new Error(`Expected ${expectedResult}, but got ${result}`);
-  }
-});
+// it('should add two numbers', function() {
+//   var expectedResult = 16;
+//   var result = operations.add(9, 7);
+//   if (result !== expectedResult) {
+//     throw new Error(`Expected ${expectedResult}, but got ${result}`);
+//   }
+// });
 
-it('shoud async multiply two numbers', function(done) {
-  var expectedResult = 12;
-  operations.multiplyAsync(4, 3, function(result) {
-    if (result !== expectedResult) {
-      throw new Error(`Expected ${expectedResult}, but got ${result}`);
-    }
-    done();
-  });
-});
+// it('shoud async multiply two numbers', function(done) {
+//   var expectedResult = 12;
+//   operations.multiplyAsync(4, 3, function(result) {
+//     if (result !== expectedResult) {
+//       throw new Error(`Expected ${expectedResult}, but got ${result}`);
+//     }
+//     done();
+//   });
+// });
 
 /*
 Немного отличается тестирование асинхронных функций.
@@ -41,3 +43,35 @@ it('shoud async multiply two numbers', function(done) {
 
 NB: Если мы не передадим функцию done в тест, тогда тест завершится раньше, чем завершится асинхронная функция.
 */
+
+// TODO:: Assert
+/*
+В предыдущей теме для верификации результата теста полученное значение сравнивалось с ожидаемым результатом: 
+
+if(result!==expectedResult){
+    throw new Error(`Expected ${expectedResult}, but got ${result}`);
+}
+
+Подобное сравнение является довольно тривиальным и фактически используется во многих тестах с небольшими изменениями. И специально чтобы упростить верификацию результатов в тестах для Node.js был создан специальный модуль - assert. Кроме того, есть различные библиотеки, которые еще называют Assertions и которые служат той же цели: should.js, expect.js и т.д. Но в данном случае мы рассмотрим работу с модулем Assert.
+
+equal
+Функция assert.equal() сравнивает два значения. Если они не равны, то генерируется ошибка. 
+*/
+
+var assert = require('assert');
+var operations = require('./operations');
+
+it('should multiply two numbers', function() {
+  var expected = 15;
+  var result = operations.multiply(3, 5);
+  assert.equal(result, expected);
+});
+
+/* Если оба значения будут равны, то мы опять же увидим в консоли, что тесты пройдены. Если же значения не будут равны, то мы увидим красные маркеры с сообщениями об ошибке */
+
+/* В противоположность этой функции функция assert.notEqual() генерирует ошибку, если оба значения равны: */
+it('should multiply two numbers', function() {
+  var expected = 15;
+  var result = operations.multiply(3, 5);
+  assert.notEqual(result, expected);
+});
