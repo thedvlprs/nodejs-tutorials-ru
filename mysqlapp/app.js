63b57045b7a3481af92eb92fe66fe1dfcb69fe00
제인도 <glitchfishzero@gmail.com>
2020-03-08 19:30:30 +0500

    feat: MySQL. Querying MySQL

--- mysqlapp/app.js
+++ mysqlapp/app.js
@@ -4,26 +4,85 @@
 /* Создание подключения
 Для создания подключения применяется метод createConnection(), который в качестве параметра принимает настройки подключения и возвращает объект, представляющий подключение.*/
 
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root', // user
+//   database: 'usersdb',
+//   password: 'root_pass' // root user password
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     return console.error('Ошибка: ' + err.message);
+//   } else {
+//     console.log('Подключение к серверу MySQL успешно установлено');
+//   }
+// });
+// // закрытие подключения
+// connection.end(function(err) {
+//   if (err) {
+//     return console.log('Ошибка: ' + err.message);
+//   }
+//   console.log('Подключение закрыто');
+// });
+
+// TODO: Выполнение запросов к MySQL
+
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'usersdb',
+//   password: '123456'
+// });
+
+// connection.query('SELECT * FROM users', function(err, results, fields) {
+//   console.log(err);
+//   console.log(results); // собственно данные
+//   console.log(fields); // мета-данные полей
+// });
+// connection.end();
+
+// Также в mysql2 определен метод execute(), который работает аналогичным образом:
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'usersdb',
+//   password: '123456'
+// });
+
+// connection.execute('SELECT * FROM users', function(err, results, fields) {
+//   console.log(err);
+//   console.log(results); // собственно данные
+//   console.log(fields); // мета-данные полей
+// });
+// connection.end();
+
+/* Параметризация запросов
+Если в запрос надо вводить данные, которые приходят извне, то для избежания sql-инъекций рекоммендуется использовать параметризацию.
+
+При параметризации вместо конкретных данных в тексте запроса ставятся плейсхолдеры - знаки вопроса, вместо которых при выполнении запроса будут вставляться собственно данные. Например, добавление данных: */
+
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root', // user
+  user: 'root',
   database: 'usersdb',
-  password: 'root_pass' // root user password
+  password: '123456'
 });
 
-connection.connect(function(err) {
-  if (err) {
-    return console.error('Ошибка: ' + err.message);
-  } else {
-    console.log('Подключение к серверу MySQL успешно установлено');
-  }
+const user = ['Kate Austen', 37];
+const sql = 'INSERT INTO users(name, age) VALUES(?, ?)';
+
+connection.query(sql, user, function(err, results) {
+  if (err) console.log(err);
+  else console.log('Данные добавлены');
 });
-// закрытие подключения
-connection.end(function(err) {
-  if (err) {
-    return console.log('Ошибка: ' + err.message);
-  }
-  console.log('Подключение закрыто');
-});
+
+connection.end();
