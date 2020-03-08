9ece044abe8f9588b6b2975a939e66c19f1f587e
제인도 <glitchfishzero@gmail.com>
2020-03-08 20:42:46 +0500

    feat: MySQL. Basic data operations

--- mysqlapp/app.js
+++ mysqlapp/app.js
@@ -88,23 +88,196 @@
 // connection.end();
 
 // TODO: Promise API
+// const mysql = require('mysql2');
+
+// const connection = mysql
+//   .createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'usersdb',
+//     password: 'Supremecoder8*'
+//   })
+//   .promise();
+
+// // получение объектов
+// connection
+//   .query('SELECT * FROM users')
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// TODO: Основные операции с данными
+
+//! Создание БД
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '123456'
+// });
+
+// connection.query('CREATE DATABASE usersdb2', function(err, results) {
+//   if (err) console.log(err);
+//   else console.log('База данных создана');
+// });
+
+// connection.end();
+
+//! Создание таблицы
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'usersdb2',
+//   password: '123456'
+// });
+
+// const sql = `create table if not exists users(
+//   id int primary key auto_increment,
+//   name varchar(255) not null,
+//   age int not null
+// )`;
+
+// connection.query(sql, function(err, results) {
+//   if (err) console.log(err);
+//   else console.log('Таблица создана');
+// });
+// connection.end();
+
+//! Добавление данных
+// const mysql = require('mysql2');
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'usersdb2',
+//   password: '123456'
+// });
+// const sql = `INSERT INTO users(name, age) VALUES('Benjamin Linus', 41)`;
+// connection.query(sql, function(err, results) {
+//   if (err) console.log(err);
+//   console.log(results);
+// });
+// connection.end();
+
+//! Добавление множества значений
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'usersdb2',
+//   password: '123456'
+// });
+
+// const users = [
+//   ['Jack Shephard', 39],
+//   ['James Ford', 39],
+//   ['Kate Austen', 38]
+// ];
+// const sql = `INSERT INTO users(name, age) VALUES ?`;
+
+// connection.query(sql, [users], function(err, results) {
+//   if (err) console.log(err);
+//   console.log(results);
+// });
+
+//! Получение данных
+// const mysql = require('mysql2');
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'usersdb2',
+//   password: '123456'
+// });
+// const sql = `SELECT * FROM users`;
+// connection.query(sql, function(err, results) {
+//   if (err) console.log(err);
+//   console.log(results);
+// });
+// connection.end();
+
+//! выведем только имя для каждого пользователя из базы данных:
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'usersdb2',
+//   password: '123456'
+// });
+
+// const sql = 'SELECT * FROM users';
+// connection.query(sql, function(err, results) {
+//   if (err) console.log(err);
+//   const users = results;
+//   for (let i = 0; i < users.length; i++) {
+//     console.log(users[i].name);
+//   }
+// });
+
+// connection.end();
+
+//! Фильтрация данных
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'usersdb2',
+//   password: '123456'
+// });
+
+// const sql = `SELECT * FROM users WHERE name=? AND age=?`;
+// const filter = ['Tom', 29];
+// connection.query(sql, filter, function(err, results) {
+//   if (err) console.log(err);
+//   console.log(results);
+// });
+
+// connection.end();
+/* Здесь запрос фактически будет выглядеть как SELECT * FROM users WHERE name="Tom" AND age=29, и в прицнипе мы могли бы напрямую ввести данные в запрос. Однако чтобы избежать sql-инъекций при передаче в запрос данных извне рекомендуется использовать параметризацию.*/
+
+//! Обновление
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'usersdb2',
+//   password: '123456'
+// });
+
+// const sql = `UPDATE users SET age=? WHERE name=?`;
+// const data = [41, 'Kate Austen'];
+// connection.query(sql, data, function(err, results) {
+//   if (err) console.log(err);
+//   console.log(results);
+// });
+
+// connection.end();
+
+// С помощью свойства affectedRows объекта results мы можем проверить, сколько строк было обновлено.
+
+//! Удаление
 const mysql = require('mysql2');
 
-const connection = mysql
-  .createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'usersdb',
-    password: 'Supremecoder8*'
-  })
-  .promise();
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'usersdb2',
+  password: '123456'
+});
 
-// получение объектов
-connection
-  .query('SELECT * FROM users')
-  .then(result => {
-    console.log(result);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+const sql = 'DELETE FROM users WHERE name=?';
+const data = ['Kate Austen']; // удаляем пользователей с именем Sam
+connection.query(sql, data, function(err, results) {
+  if (err) console.log(err);
+  console.log(results);
+});
+
+connection.end();
