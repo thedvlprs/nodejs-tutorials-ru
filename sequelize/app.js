47fddb370cf1e97b9321f4f47d206ecd1e7d23dc
제인도 <glitchfishzero@gmail.com>
2020-03-09 16:33:08 +0500

    feat: Sequelize. Defining models

--- sequelize/app.js
+++ sequelize/app.js
@@ -6,7 +6,54 @@
 // });
 
 // Подключение к postgres
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('usersdb2', 'root', '123456', {
-  dialect: 'postgres'
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('usersdb2', 'root', '123456', {
+//   dialect: 'postgres'
+// });
+
+// TODO: Определение моделей
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
 });
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('usersdb', 'root', '123456', {
+  dialect: 'mysql',
+  host: 'localhost',
+  define: {
+    timestamps: false
+  }
+});
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+sequelize
+  .sync()
+  .then(result => console.log(result))
+  .catch(err => console.log(err));
